0b190e020371878f5d49472c2c94e8210a1ea921
SWittouck <stijn.wittouck@uantwerpen.be>
2022-07-12 16:33:17 +0200

    Replace core-pipeline with core

--- src/04_demonstrate_sample/01_run_core.sh
+++ src/04_demonstrate_sample/01_run_core.sh
@@ -1,7 +1,7 @@
 # This script infers the core genome of a set of L. plantarum genomes; first the
 # full core genome, then a set of 100 core genes. 
 
-# dependencies: SCARAP commit b096905
+# dependencies: SCARAP commit 8d2ead2
 
 din_faas=../../results/lplantarum/faas
 dout=../../results/scarap_core/lplantarum
@@ -17,7 +17,7 @@ threads=16
 [ -d $dout/core100 ] || mkdir $dout/core100
 
 # infer core genome
-/usr/bin/time -v -o $dout/corefull/stats.txt scarap core-pipeline \
+/usr/bin/time -v -o $dout/corefull/stats.txt scarap core \
   $din_faas $dout/corefull -t $threads -c
-/usr/bin/time -v -o $dout/core100/stats.txt scarap core-pipeline \
-  $din_faas $dout/core100 -t $threads -c --max_genes 100
+/usr/bin/time -v -o $dout/core100/stats.txt scarap core \
+  $din_faas $dout/core100 -t $threads -c --max_cores 100

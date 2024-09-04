bac6991a761890a883ea5aebbe73d425edee123f
SWittouck <wittouck_stijn@hotmail.com>
2024-09-04 11:08:51 +0000

    Fix bugs in tonkinhill reference extraction

--- src/04_demonstrate_pan/06_evaluate_tonkinhill.R
+++ src/04_demonstrate_pan/06_evaluate_tonkinhill.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 # This script processes the pangenome benchmarks on the tonkinhill dataset. 
 
 # dependencies: R v4.1.2, tidyverse v2.0.0
@@ -10,27 +11,25 @@ fin_pans <- "../../results/scarap_pan/tonkinhill/pangenomes.csv.gz"
 fout_tonkinhill <- "../../results/scarap_pan/tonkinhill/benchmarks.csv"
 
 # extract reference pangenome from gff files
-colnames <- c(
-  "seqid", "source", "type", "start", "end", "score", "strand", "phase", 
-  "attributes"
-)
 pan_ref <- 
   list.files(din_gffs, full.names = T) %>%
-  map2(str_extract(., "[^/]+(?=.gff.gz)"), function(path, genome) {
+  map2(str_extract(., "[^/]+(?=.gff)"), function(path, genome) {
+    message(paste0("extracting reference orthogroups from ", genome))
     path %>%
-      read_tsv(
-        col_types = cols(.default = "c"), comment = "#", col_names = colnames
-      ) %>%
-      mutate(genome = {{genome}}) %>%
-      filter(type == "CDS") %>%
-      mutate(
+      read_lines() %>%
+      {.[1:which(str_detect(., "^##FASTA"))]} %>%
+      {.[! str_detect(., "^#")]} %>%
+      tibble(all = .) %>%
+      separate(all, into = str_c("X", 1:9), sep = "\t") %>%
+      select(feature = X3, attributes = X9) %>%
+      filter(feature == "CDS") %>%
+      transmute(
         gene = str_extract(attributes, "(?<=ID=)[^;]+"),
+        genome = {{genome}},
         orthogroup = str_extract(attributes, "(?<=prokka_DB.fasta:)[^;]+")
-      ) %>%
-      mutate() %>%
-      select(gene, genome, orthogroup) 
+      )
   }) %>%
-  reduce(bind_rows) 
+  reduce(.f = bind_rows)
 
 # read pangenomes 
 pans <- read_csv(fin_pans, col_types = cols())

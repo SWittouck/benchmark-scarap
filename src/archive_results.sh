a38df65f67f8891e546c2095d8186e5722ade569
SWittouck <wittouck_stijn@hotmail.com>
2024-09-10 09:16:22 +0000

    Add simpan results to archive

--- src/archive_results.sh
+++ src/archive_results.sh
@@ -7,9 +7,10 @@ tar czf benchmark_scarap_v4.tar.gz \
   scarap_pan/*/resources.csv \
   scarap_pan/*/statistics.csv \
   scarap_pan/lactobacillales_genusreps/pangenomes.csv.gz \
+  scarap_pan/simpan/pangenomes.csv.gz \
+  scarap_pan/tonkinhill/pangenomes.csv.gz \
   scarap_pan/orthobench/pangenomes.csv.gz \
   scarap_pan/parabench/pangenomes.csv.gz \
-  scarap_pan/tonkinhill/pangenomes.csv.gz \
   scarap_core/lactiplantibacillus/corefull/genes.tsv \
   scarap_core/lactiplantibacillus/core100/genes.tsv \
   scarap_core/lactiplantibacillus/pan/pangenome.tsv \
